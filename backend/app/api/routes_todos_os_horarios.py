4fa9f076cb152b03f97327a3a4bf49bfb00caf64
dudsjoww <mendeseduardo0111@gmail.com>
2025-11-01 17:24:30 -0300

    update: add functions which validates in db

--- backend/app/api/routes_todos_os_horarios.py
+++ backend/app/api/routes_todos_os_horarios.py
@@ -2,15 +2,21 @@ from fastapi import APIRouter, Depends, HTTPException  # pyright: ignore[reportM
 from sqlalchemy.orm import Session  # type: ignore
 from app.core.database import get_db
 from app.models.todos_os_horarios import TodosOsHorarios
+from app.models.tatuador import Tatuador
+from app.models.horario_de_trabalho import HorarioDeTrabalho
 from app.schemas.todos_os_horarios import TodosOsHorariosResponse, TodosOsHorariosBase
 
 router = APIRouter(prefix="/todoshorarios", tags=["horarios"])
 
+# TODO: POST não criados utilizando IDs
 
+
+#
 @router.post("/", response_model=TodosOsHorariosResponse)
 def criar_grupo_horario(
     horarioSchema: TodosOsHorariosBase, db: Session = Depends(get_db)
 ):
+    item_existe(horarioSchema, db, 0)
     novo = TodosOsHorarios(
         tatuador_id=horarioSchema.tatuador_id,
         horario_id=horarioSchema.horario_id,
@@ -50,6 +56,8 @@ def deletar_grupo_horario(grupo_horario_id: int, db: Session = Depends(get_db)):
     else:
         db.delete(grupo_horario)
         db.commit()
+        db.expire_all()
+
         return {"detail": "Grupo de horário deletado com sucesso"}
 
 
@@ -59,14 +67,50 @@ def atualizar_grupo_horario(
     horarioSchema: TodosOsHorariosBase,
     db: Session = Depends(get_db),
 ):
-    grupo_horario = (
-        db.query(TodosOsHorarios).filter(TodosOsHorarios.id == grupo_horario_id).first()
+    item_existe(horarioSchema, db, grupo_horario_id)
+
+    grupo_horario = TodosOsHorarios(
+        tatuador_id=horarioSchema.tatuador_id, horario_id=horarioSchema.horario_id
     )
-    if not grupo_horario:
-        raise HTTPException(status_code=404, detail="Grupo de horário não encontrado")
+    db.commit()
+    db.refresh(grupo_horario)
+    return grupo_horario
+
+
+def item_existe(schema, db, id=0):
+    if id:
+        if (
+            db.query(TodosOsHorarios)
+            .filter(TodosOsHorarios.id == id)
+            .first()
+            .execution_options(populate_existing=True)
+        ):
+            raise HTTPException(
+                status_code=404, detail="Grupo de Horarios não encontrado"
+            )
+
+    db_search_horario = (
+        db.query(HorarioDeTrabalho)
+        .filter(HorarioDeTrabalho.id == schema.horario_id)
+        .first()
+    )
+    db_search_tatuador = (
+        db.query(Tatuador).filter(Tatuador.id == schema.tatuador_id).first()
+    )
+    # Valida se não existe tatuador ou horario
+    if not db_search_horario or not db_search_tatuador:
+        raise HTTPException(
+            status_code=404, detail="Tatuador ou Horario de Trabalho não encontrado"
+        )
+    # Valida se já existe a relação que solicita
+    elif (
+        db.query(TodosOsHorarios)
+        .filter(
+            schema.tatuador_id == TodosOsHorarios.tatuador_id
+            and schema.horario_id == TodosOsHorarios.horario_id
+        )
+        .first()
+    ):
+        raise HTTPException(status_code=404, detail="Relação já feita")
     else:
-        grupo_horario.tatuador_id = horarioSchema.tatuador_id
-        grupo_horario.horario_id = horarioSchema.horario_id
-        db.commit()
-        db.refresh(grupo_horario)
-        return grupo_horario
+        return
